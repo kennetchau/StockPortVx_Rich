fb54259072d523a20ea1d7b49ed099ca6ff3ad57
kenneth <gitlab.natural668@passmail.net>
2024-07-21 13:32:58 -0400

    Fixing files structures

--- app.py
+++ app.py
@@ -4,15 +4,20 @@ from rich.live import Live
 from rich.console import Console
 from rich.table import Table
 
+class Portfolio:
+    def __init__(self, df:pd.DataFrame):
+        df['Book Cost'] = df['Quantity'] * df['Cost']
+        dfStockPortOver = df.drop(columns = ['Date']).groupby(by = "Symbol").sum().reset_index(drop=False)
+        dfStockPortOver = dfStockPortOver[["Symbol", "Quantity", "Book Cost"]]
+        self.dfStockPortOver = dfStockPortOver.sort_values(by = "Book Cost", ascending = False)
+
+    def returnTable(self)->pd.DataFrame:
+        return self.dfStockPortOver
+
+
 def read_json(path:str)->pd.DataFrame:
     df1 = pd.read_json(path)
     return df1
-
-def returnTables(df:pd.DataFrame)->list:
-    df['Book Cost'] = df['Quantity'] * df['Cost']
-    dfStockPortOver = df.drop(columns = ['Date']).groupby(by = "Symbol").sum().reset_index(drop=False)
-    dfStockPortOver = dfStockPortOver[["Symbol", "Quantity", "Book Cost"]]
-    return dfStockPortOver.sort_values(by = "Book Cost", ascending = False)
 
 def drawTable(df:pd.DataFrame, title:str)->Table:
     table = Table(title = title, title_justify = 'left')
@@ -33,8 +38,8 @@ def main():
 
     # read the json for the stock Portfolio
     df = read_json(dataPath)
-    dfStockPortOver = returnTables(df)
-    table = drawTable(dfStockPortOver, "Stock Portfolio")
+    dfStockPortOver = Portfolio(df)
+    table = drawTable(dfStockPortOver.returnTable(), "Stock Portfolio")
 
     console = Console()
     console.print(table)
